8843a5980358b11d0f2367bd34dd17bb9f554b52
johnray100 <jfornias10@gmail.com>
2024-12-12 17:46:17 +0800

    updates

---
+++ src/test/java/jay/springboot_crud_sweet_alert/SpringbootCrudSweetAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package jay.springboot_crud_sweet_alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudSweetAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
